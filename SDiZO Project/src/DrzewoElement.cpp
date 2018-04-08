e8140e9072d256b9d74ae9048bdf731a07d73f47
kuba1 <jakub.deren97@gmail.com>
2018-04-08 14:26:42 +0200

    Dodane dwie klasy

---
+++ SDiZO Project/src/DrzewoElement.cpp
@@ -0,0 +1,18 @@
+/*
+ * DrzewoElement.cpp
+ *
+ *  Created on: 8 kwi 2018
+ *      Author: kuba
+ */
+
+#include "DrzewoElement.h"
+
+DrzewoElement::DrzewoElement() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DrzewoElement::~DrzewoElement() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ SDiZO Project/src/DrzewoElement.h
@@ -0,0 +1,17 @@
+/*
+ * DrzewoElement.h
+ *
+ *  Created on: 8 kwi 2018
+ *      Author: kuba
+ */
+
+#ifndef DRZEWOELEMENT_H_
+#define DRZEWOELEMENT_H_
+
+class DrzewoElement {
+public:
+	DrzewoElement();
+	virtual ~DrzewoElement();
+};
+
+#endif /* DRZEWOELEMENT_H_ */
